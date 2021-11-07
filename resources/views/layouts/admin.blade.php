17fb9c91e5d74b9dd97dd5264887ab5037f71839
Tilis Tiadi <tiliztiadi@gmail.com>
2021-11-07 16:21:10 +0700

    Fix Conflict Admin

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -2,7 +2,6 @@
 <html lang="en">
 
 <head>
-<<<<<<< HEAD
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Dashboard Admin</title>
@@ -29,23 +28,6 @@
     <link rel="stylesheet" href={{ asset("adminlte/plugins/daterangepicker/daterangepicker.css") }}>
     <!-- summernote -->
     <link rel="stylesheet" href={{ asset("adminlte/plugins/summernote/summernote-bs4.min.css") }}>
-=======
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="csrf-token" content="{{ csrf_token() }}" />
-  <title>Dashboard Admin</title>
-
-  <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href={{ asset("adminlte/plugins/fontawesome-free/css/all.min.css") }}>
-  <!-- Theme style -->
-  <link rel="stylesheet" href={{ asset("adminlte/dist/css/adminlte.min.css") }}>
-  <!-- overlayScrollbars -->
-  <link rel="stylesheet" href={{ asset("adminlte/plugins/overlayScrollbars/css/OverlayScrollbars.min.css") }}>
-  <!-- Daterange picker -->
-  <link rel="stylesheet" href={{ asset("adminlte/plugins/daterangepicker/daterangepicker.css") }}>
->>>>>>> c80bf709ad302e7d04702dd81596178b06c59b4c
 </head>
 
 <body class="hold-transition sidebar-mini layout-fixed">
@@ -140,7 +122,6 @@
         </aside>
         <!-- /.control-sidebar -->
     </div>
-<<<<<<< HEAD
     <!-- ./wrapper -->
 
     <!-- jQuery -->
@@ -179,56 +160,6 @@
     <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
     <script src={{ asset("adminlte/dist/js/pages/dashboard.js") }}></script>
     @yield('datatable-script')
-=======
-    <!-- /.content-header -->
-
-    <!-- Main content -->
-    <section class="content">
-
-        <!-- Default box -->
-        @yield('box')
-        <!-- /.card -->
-
-      </section>
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
-  <footer class="main-footer">
-    <strong>Copyright &copy; 2021 <a href="#">Jualan-Ikan</a>.</strong>
-    All rights reserved.
-  </footer>
-
-  <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
-  </aside>
-  <!-- /.control-sidebar -->
-</div>
-<!-- ./wrapper -->
-
-<!-- jQuery -->
-<script src={{ asset("adminlte/plugins/jquery/jquery.min.js") }}></script>
-<!-- jQuery UI 1.11.4 -->
-<script src={{ asset("adminlte/plugins/jquery-ui/jquery-ui.min.js") }}></script>
-<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-<script>
-  $.widget.bridge('uibutton', $.ui.button)
-</script>
-<!-- Bootstrap 4 -->
-<script src={{ asset("adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js") }}></script>
-<!-- daterangepicker -->
-<script src={{ asset("adminlte/plugins/moment/moment.min.js") }}></script>
-<script src={{ asset("adminlte/plugins/daterangepicker/daterangepicker.js") }}></script>
-<!-- Summernote -->
-<script src={{ asset("adminlte/plugins/summernote/summernote-bs4.min.js") }}></script>
-<!-- overlayScrollbars -->
-<script src={{ asset("adminlte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js") }}></script>
-<!-- AdminLTE App -->
-<script src={{ asset("adminlte/dist/js/adminlte.js") }}></script>
-<!-- AdminLTE for demo purposes -->
-<script src={{ asset("adminlte/dist/js/demo.js") }}></script>
-@yield('datatable-script')
->>>>>>> c80bf709ad302e7d04702dd81596178b06c59b4c
 </body>
 
 </html>
