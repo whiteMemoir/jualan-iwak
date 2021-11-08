cc4af179cb9449a506cdbb42cb50f8223e8300f8
Tilis Tiadi <tiliztiadi@gmail.com>
2021-11-08 19:59:11 +0700

    Menambahkan Alamat Kantor

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,5 +1,7 @@
-<div class="body-flex">
+<div class="body-flex py-md-3 border" style="box-shadow: none; padding-bottom: 25px !important;">
+    <h2 class="mb-3">Alamat Kantor</h2>
+    <p class="text-center">Jl. HEA Mokodompit (Depan Gerbang Universitas Haluoleo) <br> Kel. Kambu Kec. Kambu, Kota Kendari <br> Sulawesi Tenggara 93561 </p>
     <footer>
-
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1183.276053391567!2d122.5168962237523!3d-4.010065993208102!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNMKwMDAnMzUuNSJTIDEyMsKwMzEnMDEuNiJF!5e0!3m2!1sid!2sid!4v1636374830625!5m2!1sid!2sid" width="100%" height="100%" style="border:0;" loading="lazy"></iframe>
     </footer>
 </div>
