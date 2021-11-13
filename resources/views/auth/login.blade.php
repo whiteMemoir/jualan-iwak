aa8988ccd35f6a0801c0a774d7a32d12ebe6cb50
Tilis Tiadi <tiliztiadi@gmail.com>
2021-11-13 20:33:32 +0700

    Ganti Halaman Login - Nanti Ganti Lagi

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-6">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="card-header text-center" style="font-weight: bold;">{{ __('Login') }} Admin</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
